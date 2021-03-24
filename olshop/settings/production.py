c25f8f4f24c8e13e7bb020dbb18a0a05113c4fe7
IanTheSecond <26853387+ianthesecond@users.noreply.github.com>
2021-03-24 19:16:21 +0300

    tweaked production settings

--- olshop/settings/production.py
+++ olshop/settings/production.py
@@ -10,5 +10,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
+EMAIL_USE_TLS = True 
+EMAIL_HOST = 'smpt.gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
