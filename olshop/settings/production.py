c711fd46783ffaf82472dbb691ede9a3403f8fc5
IanTheSecond <26853387+ianthesecond@users.noreply.github.com>
2021-03-25 08:00:27 +0300

    procfile changes and production settings

--- olshop/settings/production.py
+++ olshop/settings/production.py
@@ -8,6 +8,12 @@ ALLOWED_HOSTS += 'liannie.herokuapp.com'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 EMAIL_USE_TLS = True 
@@ -15,3 +21,7 @@ EMAIL_HOST = 'smpt.gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
