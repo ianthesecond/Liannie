c07dab7a4d671c87424b6267ca7dc9014747d1f0
IanTheSecond <26853387+ianthesecond@users.noreply.github.com>
2021-04-21 23:36:23 +0300

    re-enabled cloudinary functionalities

--- olshop/settings/production.py
+++ olshop/settings/production.py
@@ -4,11 +4,11 @@ from decouple import config
 import cloudinary
 import cloudinary_storage
 
-# INSTALLED_APPS += [
-#     # Media Cloudinary
-#     'cloudinary',
-#     'cloudinary_storage',
-# ]
+INSTALLED_APPS += [
+    # Media Cloudinary
+    'cloudinary',
+    'cloudinary_storage',
+]
 
 DEBUG = True
 
@@ -22,12 +22,12 @@ MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary stuff
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME', default=""),
-#     'API_KEY': config('CLOUDINARY_API_KEY', default=""),
-#     'API_SECRET': config('CLOUDINARY_API_SECRET', default=""),
-# }
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME', default=""),
+    'API_KEY': config('CLOUDINARY_API_KEY', default=""),
+    'API_SECRET': config('CLOUDINARY_API_SECRET', default=""),
+}
 
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=600, ssl_require=True))
