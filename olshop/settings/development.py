29f68b0611c56f52b167fa67fe3d0d8055c56ed9
IanTheSecond <26853387+ianthesecond@users.noreply.github.com>
2021-04-21 21:35:38 +0300

    fixed email

--- olshop/settings/development.py
+++ olshop/settings/development.py
@@ -4,3 +4,4 @@ DEBUG = True
 
 ALLOWED_HOSTS += ['127.0.0.1']
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- olshop/settings/production.py
+++ olshop/settings/production.py
@@ -33,7 +33,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES['default'].update(dj_database_url.config(conn_max_age=600, ssl_require=True))
 
 EMAIL_USE_TLS = True 
-EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
